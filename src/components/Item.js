441e78e4084097bbaadb55ad4539d2523dbccf3d
Artem-Prots <protsartem60@gmail.com>
2024-03-25 22:40:54 +0300

    version1

---
+++ src/components/Item.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Item = () => {
+    // Массив объектов с загадками
+  const riddles = [
+    {
+        id: 1,
+        description: 'Загадка 1: Шли муж с женой, брат с сестрой да муж с шурином. Сколько всего человек?',
+        option1: '3',
+        option2: '4',
+        option3: '5',
+        option4: '2',
+        correct: 1
+    },
+    {
+        id: 2,
+        description: 'Загадка 2: Что становится больше, когда его переворачиваешь?',
+        option1: 'Яма',
+        option2: 'Тень',
+        option3: 'Цифра "6"',
+        option4: 'Луна',
+        correct: 3
+    },
+    {
+        id: 3,
+        description: 'Загадка 3: Сколько существует цифр?',
+        option1: 'Десять',
+        option2: 'Бесконечность',
+        option3: 'Никто не знает',
+        option4: 'Девять',
+        correct: 1
+    }
+];
+
+return (
+    <div>
+        <h2>Загадки</h2>
+        {riddles.map(riddle => (
+            <div key={riddle.id}>
+                <p className="question">{riddle.description}</p>
+                <input type="radio" name={riddle.id} id={riddle.id + '.1'}></input>{riddle.option1}<br/>
+                <input type="radio" name={riddle.id} id={riddle.id + '.2'}></input>{riddle.option2}<br/>
+                <input type="radio" name={riddle.id} id={riddle.id + '.3'}></input>{riddle.option3}<br/>
+                <input type="radio" name={riddle.id} id={riddle.id + '.4'}></input>{riddle.option4}
+            </div>
+        ))}
+    </div>
+);
+}
+
+export default Item
